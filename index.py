871983f18e0e272f11253eac6e07e1cae954b8bb
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-18 14:55:31 -0500

    Update index.py

--- index.py
+++ index.py
@@ -29,10 +29,75 @@ title = html.Div(
     ], className='card-title'
 )
 
-df = px.data.gapminder()
+def cryptofees():
+    fees_requests = requests.get("https://cryptofees.info/api/v1/fees")
+    json_data = fees_requests.json()
 
-fig = px.bar(df, x="continent", y="pop", color="continent",
-  animation_frame="year", animation_group="country", range_y=[0,4000000000])
+    _df = pd.json_normalize(json_data['protocols'],
+                            record_path=['fees'],
+                            meta=['name', 'tokenTicker', 'id', 'category', 'blockchain'],
+                            errors='ignore')
+
+    _df.loc[_df['blockchain'].isna(), 'blockchain'] = _df.name
+    _df.id.replace(id_dict, inplace=True)
+
+    return _df
+
+
+id_dict = {
+    'eth': 'Ethereum',
+    'bsc': 'Binance Smart Chain',
+    'uniswap-v3': 'Uniswap V3',
+    'uniswap-v2': 'Uniswap V2',
+    'sushiswap': 'Sushiswap',
+    'compound': 'Compound',
+    'btc': 'Bitcoin',
+    'aave-v2': 'Aave V2',
+    'avalanche': 'Avalanche',
+    'maker': 'MakerDAO',
+    'aave-v2-avalanche-proto': 'Aave V2 - Avalanche',
+    'terraswap': 'Terraswap',
+    'balancerv2': 'Balance V2', 'pangolin': 'Pangolin', 'quickswap': 'Quickswap', 'ens': 'Ethereum Naming Service',
+    'sushiswap-polygon': 'Sushiswap - Polygon',
+    'arbitrum-one': 'Arbitrum', 'sushiswap-arbitrum': 'Sushiswap - Arbitrum',
+    'osmosis': 'Osmosis', 'synthetix-mainnet': 'Synthetix',
+    'polygon': 'Polygon', 'bancor': 'Bancor', 'fantom': 'Fantom', 'uniswap-arbitrum': 'Uniswap - Arbitrum',
+    'ada': 'Cardano',
+    'balancerv2-polygon': 'Balancer - Polygon',
+    'liquity': 'Liquity', 'balancer-v1': 'Balancer V1', 'dfyn': 'Dfyn',
+    'hop-arbitrum': 'Hop - Arbitrum', 'tornado': 'Tornado', 'terra': 'Terra',
+    'hop-polygon': 'Hop - Polygon', 'ren': 'Ren', 'doge': 'Doge', 'visor': 'Visor',
+    'xdai': 'xDAI', 'mstable': 'mStable', 'balancerv2-arbitrum': 'Balancer V2 - Arbitrum',
+    'aave-v1': 'Aave V1', 'zilliqa': 'Zilliqa', 'xtz': 'Tezos', 'xrp': 'Ripple', 'bsv': 'Bitcoin Satoshi Vision',
+    'ltc': 'Litecoin', 'polymarket': 'Polymarket',
+    'sushiswap-fantom': 'Sushiswap - Fantom', 'swapr-arbitrum': 'Swapr - Arbitrum', 'honeyswap': 'HoneySwap',
+    'polkadot': 'Polkadot', 'xlm': 'Stellar',
+    'uniswap-v1': 'Uniswap V1', 'hop-xdai': 'Hop - xDAI', 'linkswap': 'Linkswap', 'aave-v2-amm': 'Aave V2',
+    'xmr': 'Monero', 'bch': 'Bitcoin Cash', 'swapr-xdai': 'Swapr - xDAI',
+    'tbtc': 'tBTC', 'swapr-ethereum': 'Swapr - Ethereum', 'kusama': 'Kusama', 'zerox': '0x',
+    'aave-v2-polygon-proto': 'Aave V2 - Polygon'
+}
+column_dict = {
+    'id': '',
+    'name': '',
+    'fee': 'Fees',
+}
+df = cryptofees()
+bar1_data = df.groupby('id').sum().reset_index().nlargest(10, 'fee').sort_values(by='fee', ascending=False)
+df = df[df.id.isin(bar1_data.id)]
+
+fig = px.bar(df,
+             y="id",
+             x="fee",
+             color="name",
+             # height=1000,
+             orientation='h',
+             labels=column_dict,
+             animation_frame="date",
+             range_x=[0, df.fee.max() * 1.1],
+             category_orders={"id": np.flip(df[['id', 'fee']].sort_values(by=['fee'], ascending=True)['id'].unique()),
+                              "date": df.date.sort_values(ascending=True)}
+             )
 
 first_card = dbc.Card(
     dcc.Graph(figure=fig,
